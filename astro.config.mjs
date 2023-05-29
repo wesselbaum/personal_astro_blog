1df8a8235b4d7dfc285470febdea8a9988c8639c
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-05-29 13:05:40 +0200

    github workflow added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://wesselbaum.github.io/personal_astro_blog',
+    base: '/personal_astro_blog',
+});
