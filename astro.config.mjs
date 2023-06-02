962bb0b89df84b3348fa2f2ee98ad5a5a8df0107
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-06-02 16:55:21 +0200

    styling und cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wesselbaum.github.io/personal_astro_blog',
-    base: '/personal_astro_blog',
+  site: 'https://wesselbaum.github.io/personal_astro_blog',
+  base: '/personal_astro_blog',
+  integrations: [preact()]
 });
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,21 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+interface GreetingProps {
+    messages: string[]
+}
+export default function Greeting({messages}: GreetingProps) {
+
+    const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+    const [greeting, setGreeting] = useState(messages[0]);
+
+    return (
+        <div>
+            <h3>{greeting}! Thank you for visiting!</h3>
+            <button onClick={() => setGreeting(randomMessage())}>
+                New Greeting
+            </button>
+        </div>
+    );
+}
